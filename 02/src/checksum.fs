a4ef421ccc70b5dea1c621bce03b6dcfb9b4bb5d
Andrew Beyer <beyer.andrew@gmail.com>
2017-12-10 23:06:07 -0800

    day 2 part 2

--- 02/src/checksum.fs
+++ 02/src/checksum.fs
@@ -16,8 +16,25 @@ let checksums (f:int[]->int) : seq<int> =
 let minMaxDifference arr = Array.max arr - Array.min arr
 
 
+let twoCombinations (arr:'a[]) =
+    let rec comb (acc:('a*'a) list) (l:'a list) (x:'a) (r:'a list) : ('a*'a) list =
+        match r with
+            | hd::tl -> (comb (acc @ (List.map (fun y -> (x, y)) l) @ (List.map (fun y -> (y, x)) l)) (x::l) hd tl)
+            | _ -> acc @ (List.map (fun y -> (x, y)) l) @ (List.map (fun y -> (y, x)) l)
+    let hd::tl = List.ofArray(arr)
+    comb [] [] hd tl
+
+
+let divideResult arr =
+    let rec divide l =
+        match l with
+            | (x,y)::tl when x % y = 0 -> x / y
+            | _::tl -> divide tl
+    divide (twoCombinations arr)
+
+
 [<EntryPoint>]
 let main args =
-    let total = (checksums minMaxDifference) |> Seq.sum
+    let total = (checksums divideResult) |> Seq.sum
     printfn "%i" total
     0
